8fbd9fe64659f2ca6b3fcdef7d025decf550d44f
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-11-20 20:22:36 +0300

    added express

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = 4000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'out')));
 
 app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'out', 'index.html'));
 });
 
 app.listen(PORT, () => {

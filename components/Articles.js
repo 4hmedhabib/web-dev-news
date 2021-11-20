114d88c415965ec156ac7bca82ded3f6e78d748f
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-11-20 19:03:47 +0300

    updated

--- components/Articles.js
+++ components/Articles.js
@@ -1,7 +1,6 @@
 import Article from './Article';
 
 const Articles = ({ articles }) => {
-	console.log(articles);
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
 			{articles.map((article) => {
